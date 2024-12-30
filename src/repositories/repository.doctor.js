12f3b370a6b3a059fd4a75041e03cefdf933fb15
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2024-12-29 21:29:44 -0300

    Criado Banco de Dados, adicionado Classe para conexão e Realização do primeiro GET atraves da repository

--- src/repositories/repository.doctor.js
+++ src/repositories/repository.doctor.js
@@ -1,13 +1,12 @@
+import { query } from "../database/sqlite.js";
+
 async function Listar (){
-        const doctor = [
-            { id: 1, name: "Leonardo", specialty: "Cardiologia", icon : "M"},
-            { id: 2, name: "João", specialty: "Dermatologia", icon : "M"},
-            { id: 3, name: "Maria", specialty: "Ortopedia", icon : "F"},
-            { id: 4, name: "Pedro", specialty: "Oftalmologia", icon : "M"},
-            { id: 5, name: "Ana", specialty: "Ginecologia", icon : "F"},
-            { id: 6, name: "Lucas", specialty: "Urologia", icon : "M"},
-        ];
-        return doctor;
+
+    let sql = "SELECT * FROM doctors ORDER BY id_doctor";    
+
+    const doctors = await query(sql, []);
+    
+    return doctors;
 }
 
 export default {Listar};
